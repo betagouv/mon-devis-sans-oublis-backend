e2f190777f8578b9e3a71fc24ea110c6736585f3
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2024-11-08 11:21:25 +0100

    chore: add Password access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
+# Main controller for the application
 class ApplicationController < ActionController::Base
+  before_action :authenticate, if: -> { ENV.key?("MDSO_SITE_PASSWORD") }
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic("Administration") do |username, password|
+      username == "mdso" && password == ENV.fetch("MDSO_SITE_PASSWORD")
+    end
+  end
 end
