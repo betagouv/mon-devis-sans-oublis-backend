3c84d257ebd30a3d6af12269f28fb3edbec2b1f9
Martin Letellier (Beta.gouv) <dinum-327867@martinlnsoublis.home>
2024-11-08 16:24:47 +0100

    V0 quote validator on admin

--- lib/quote_validator.rb
+++ lib/quote_validator.rb
@@ -13,10 +13,77 @@ class QuoteValidator
 
   def validate!
     @errors = []
+    @warning = []
 
-    @errors << "SIRET number is missing" if @quote[:siret].blank?
+    validate_admin
+    validate_works
 
     valid?
+  end
+
+  # doit valider les mentions administratives du devis
+  def validate_admin
+    # mention devis présente ou non, quote[:devis] est un boolean
+    @errors << "devis_manquant" if not @quote[:devis]
+    @errors << "numero_devis_manquant" if not @quote[:numero_devis].blank?
+    validate_dates
+    validate_pro
+    validate_client
+  end
+
+  # date d'emission, date de pré-visite (CEE uniquement ?), validité (par défaut 3 mois -> Juste un warning), Date de début de chantier (CEE uniquement)
+  def validate_dates
+  end
+
+  # doit valider les mentions administratives associées à l'artisan
+  def validate_pro
+    @pro = @quote[:pro]
+    @errors << "pro_raison_sociale_manquant" if @pro[:raison_sociale].blank?
+    @errors << "pro_forme_juridique_manquant" if @pro[:forme_juridique].blank?
+    @errors << "tva_manquant" if @pro[:numero_tva].blank?
+    # TODO check format tva : FR et de 11 chiffres (une clé informatique de 2 chiffres et le numéro SIREN à 9 chiffres de l'entreprise) 
+
+    # TODO rajouter une condition si personne physique professionnelle et dans ce cas pas de SIRET nécessaire
+    @errors << "capital_manquant" if @pro[:capital].blank?
+    @errors << "siret_manquant" if @pro[:siret].blank?
+    # beaucoup de confusion entre SIRET (14 chiffres pour identifier un etablissement) et SIREN (9 chiffres pour identifier une entreprise)
+    @errors << "siret_format_erreur" if @pro[:siret].length != 14 && @pro[:siret].length > 0
+    validate_pro_address
+
+  end
+
+  # doit valider les mentions administratives associées au client
+  def validate_client
+    @client = @quote[:client]
+    @errors << "client_prenom_manquant" if @client[:prenom].blank?
+    @errors << 'client_nom_manquant' if @client[:nom].blank?
+    validate_client_address
+  end
+
+  # vérifier la présence de l'adresse du client. + Warning pour préciser que l'adresse de facturation = adresse de chantier si pas de présence
+  def validate_client_address
+    client_address = @client[:adresse]
+    validate_address(client_address)
+
+    site_address = @client[:adresse_chantier]
+    if site_address.blank?
+      @warning << "chantier_facturation_idem"
+    else
+      validate_address(site_address)
+    end
+  end
+
+  def validate_pro_address
+    address = @pro[:adresse]
+    validate_address(address)
+  end
+
+  def validate_address(address)
+
+  end
+
+  # doit valider les critères techniques associés aux gestes présents dans le devis
+  def validate_works
   end
 
   def valid?
