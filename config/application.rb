34d2c0a38adff088898ae75ea831beaaa2de021d
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2024-11-19 16:33:03 +0100

    fix: autoloading libs

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,8 @@ module MesDevisSansOublis
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
