61597d19f1513b8be16ee5a20a5735aac97d47d9
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2024-12-03 19:44:16 +0100

    fix: locales to only French

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,8 @@ module MesDevisSansOublis
     config.load_defaults 7.2
 
     config.i18n.default_locale = :fr
-    config.i18n.fallbacks = [:en]
+    config.i18n.fallbacks = [:fr]
+    config.i18n.available_locales = [:fr]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
