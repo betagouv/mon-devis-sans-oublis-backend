a89b22f5b40d523ab1ab072c47ac8e2fa9369c32
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2024-12-05 15:28:01 +0100

    fix: test factories

--- spec/factories/quote_checks.rb
+++ spec/factories/quote_checks.rb
@@ -2,17 +2,23 @@
 
 FactoryBot.define do
   factory :quote_check do
-    file { nil }
-    profile { "MyString" }
+    file factory: %i[quote_file]
+
+    profile { "artisan" }
     text { "MyText" }
     anonymised_text { "MyText" }
-    naive_attributes { "" }
-    qa_attributes { "" }
+
+    naive_attributes { {} }
     naive_version { "MyString" }
-    qa_version { "MyString" }
-    read_attributes { "" }
-    validation { "" }
-    validation_version { "MyString" }
+    qa_attributes { {} }
+    qa_result { {} }
+    qa_version { QuoteReader::Qa::VERSION }
+    read_attributes { {} }
+
+    validation_errors { {} }
+    validation_version { QuoteValidator::Global::VERSION }
+
     started_at { "2024-11-27 11:33:58" }
+    finished_at { "2024-11-27 11:32:58" }
   end
 end
--- spec/factories/quote_files.rb
+++ spec/factories/quote_files.rb
@@ -3,7 +3,16 @@
 FactoryBot.define do
   factory :quote_file do
     filename { "MyString" }
+    hexdigest { "file_fixture quote.pdf" }
+    content_type { "application/pdf" }
     uploaded_at { "2024-11-26 19:35:07" }
-    hexdigest { "MyString" }
+
+    after(:build) do |quote_check|
+      quote_check.file.attach(
+        io: Rails.root.join("spec/fixtures/files/quote_files/Devis_test.pdf").open,
+        filename: "Devis_test.pdf",
+        content_type: "application/pdf"
+      )
+    end
   end
 end
