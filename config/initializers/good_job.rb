0534e3d374b2b873f046fbf735ed2472eabe4239
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2024-12-19 17:37:47 +0100

    fix: GoodJob default locale

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
+  Rails.application.configure do |config|
+    config.good_job.dashboard_default_locale = Rails.application.confi.i18n.default_locale
+  end
+
   GoodJob::Engine.middleware.use(Rack::Auth::Basic) do |username, password|
     ActiveSupport::SecurityUtils.secure_compare(ENV.fetch("GOOD_JOB_USERNAME"), username) &
       ActiveSupport::SecurityUtils.secure_compare(ENV.fetch("GOOD_JOB_PASSWORD"), password)
