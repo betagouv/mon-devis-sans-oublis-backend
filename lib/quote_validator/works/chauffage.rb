9741f90578a0ccdde4be706eae2b4bcc89340f2f
Martin Letellier (Beta.gouv) <dinum-327867@Martin-Letellier---Mon-Devis-Sans-Oublis.local>
2024-12-20 10:41:43 +0100

    fix chauffage clé erreurs

--- lib/quote_validator/works/chauffage.rb
+++ lib/quote_validator/works/chauffage.rb
@@ -9,15 +9,15 @@ module QuoteValidator
     # Validator for the Work
     class Chauffage < Base
       def validate_chauffage(geste)
-        add_error("puissance_manquant", geste) if geste[:puissance].blank?
-        add_error("marque_isolation_manquant", geste) if geste[:marque].blank?
-        add_error("reference_isolation_manquant", geste) if geste[:reference].blank?
-        add_error("etas_chauffage_manquant", geste) if geste[:ETAS].blank? # en %
+        add_error("chauffage_puissance_manquant", geste) if geste[:puissance].blank?
+        add_error("chauffage_marque_manquant", geste) if geste[:marque].blank?
+        add_error("chauffage_reference_manquant", geste) if geste[:reference].blank?
+        add_error("chauffage_etas_manquant", geste) if geste[:ETAS].blank? # en %
 
         # TODO: à challenger
         return if geste[:remplacement_chaudiere_condensation]
 
-        add_error("remplacement_chaudiere_condensation_manquant", geste)
+        add_error("chauffage_remplacement_chaudiere_condensation_manquant", geste)
       end
 
       # rubocop:disable Metrics/AbcSize
@@ -28,12 +28,12 @@ module QuoteValidator
         validate_chauffage(geste)
 
         # buche, granulé, copeaux ...
-        add_error("type_combustible_chaudiere_manquant", geste) if geste[:type_combustible].blank?
-        add_error("type_chargement_manquant", geste) if geste[:type_chargement].blank? # manuelle ou auto
+        add_error("chaudiere_type_combustible_manquant", geste) if geste[:type_combustible].blank?
+        add_error("chaudiere_type_chargement_manquant", geste) if geste[:type_chargement].blank? # manuelle ou auto
         # externe/interne, neuf/existant, Textile/maconner
-        add_error("type_silo_manquant", geste) if geste[:type_silo].blank?
-        add_error("contenance_silo_manquant", geste) if geste[:contenance_silo].blank?
-        add_error("contenance_silo_trop_petit", geste, type: "wrong") if geste[:contenance_silo].present? &&
+        add_error("chaudiere_type_silo_manquant", geste) if geste[:type_silo].blank?
+        add_error("chaudiere_contenance_silo_manquant", geste) if geste[:contenance_silo].blank?
+        add_error("chaudiere_contenance_silo_trop_petit", geste, type: "wrong") if geste[:contenance_silo].present? &&
                                                                          geste[:contenance_silo] < 225
 
         # TODO: V1 : Valeur EtAS :
@@ -42,29 +42,29 @@ module QuoteValidator
 
         # Si label, pas besoin de vérifier les emissions
         unless geste[:label_flamme_verte]
-          add_error("emission_CO_chaudiere_manquant", geste) if geste[:emission_CO].blank?
+          add_error("chaudiere_emission_CO_manquant", geste) if geste[:emission_CO].blank?
           # Emission monoxyde de carbone rapportée (CO) à 10% d’O2 (mg/Nm3)
           # TODO V1 ≤600mg/Nm3 pour manuelle et ≤400mg/Nm3 pour automatique)
 
-          add_error("emission_COG_chaudiere_manquant", geste) if geste[:emission_COG].blank?
+          add_error("chaudiere_emission_COG_manquant", geste) if geste[:emission_COG].blank?
           # Emission de composés organiques volatiles (COG) (mg/Nm3) rapportée à 10% d’O2
           # TODO V1 :(≤ 20mg/Nm3 pour manuelle ≤16mg/Nm3 pour automatique)
 
-          add_error("emission_particule_chaudiere_manquant", geste) if geste[:emission_particule].blank?
+          add_error("chaudiere_emission_particule_manquant", geste) if geste[:emission_particule].blank?
           # Emission de particules (mg/Nm3)
           # todo V1 : (≤40 pour manuelle et ≤30 pour automatique)
 
-          add_error("emission_nox_chaudiere_manquant", geste) if geste[:emission_Nox].blank?
+          add_error("chaudiere_emission_nox_manquant", geste) if geste[:emission_Nox].blank?
           # Emissions d’oxydes d’azote (NOx) rapporté à 10% d’O2 (mg/Nm3)
           # TODO (≤200 pour les deux)
 
         end
 
         # Régulateur. TODO : A challenger si on met en V0 ?
-        add_error("marque_regulateur_manquant", geste) if geste[:marque_regulateur].blank?
-        add_error("reference_regulateur_manquant", geste) if geste[:reference_regulateur].blank?
+        add_error("chaudiere_marque_regulateur_manquant", geste) if geste[:marque_regulateur].blank?
+        add_error("chaudiere_reference_regulateur_manquant", geste) if geste[:reference_regulateur].blank?
         # TODO: V1 : Classe IV selon classification européenne
-        add_error("classe_regulateur_manquant", geste) if geste[:classe_regulateur].blank?
+        add_error("chaudiere_classe_regulateur_manquant", geste) if geste[:classe_regulateur].blank?
       end
       # rubocop:enable Metrics/PerceivedComplexity
       # rubocop:enable Metrics/MethodLength
@@ -76,26 +76,26 @@ module QuoteValidator
       def validate_poele_insert(geste)
         validate_chauffage(geste)
 
-        add_error("type_combustible_poele_manquant", geste) if geste[:type_combustible].blank? # buche, granulé
-        add_error("rendement_energetique_manquant", geste) if geste[:rendement_energetique].blank?
+        add_error("poele_type_combustible_manquant", geste) if geste[:type_combustible].blank? # buche, granulé
+        add_error("poele_rendement_energetique_manquant", geste) if geste[:rendement_energetique].blank?
 
         # TODO: V1 : vérifier valeur ETAS (ɳs) (≥ 80% pour granulé, ≥ 65% pour bûches)
 
         return if geste[:label_flamme_verte]
 
-        add_error("emission_CO_poele_manquant", geste) if geste[:emission_CO].blank?
+        add_error("poele_emission_CO_manquant", geste) if geste[:emission_CO].blank?
         # Emission de monoxyde de carbone rapporté à 13% d’O2) (mg/Nm3)
         # TODO V1 : (≤1500 pour bûches ≤ 300 pour granulé)
 
-        add_error("emission_COG_poele_manquant", geste) if geste[:emission_COG].blank?
+        add_error("poele_emission_COG_manquant", geste) if geste[:emission_COG].blank?
         # Emission de composés organiques Volatile (COG) rapporté à 13% d’O2(mgC/Nm3)
         # TODO V1 : (≤120 si bûches ≤ 60 si granulé)
 
-        add_error("emission_particule_poele_manquant", geste) if geste[:emission_particule].blank?
+        add_error("poele_emission_particule_manquant", geste) if geste[:emission_particule].blank?
         # Emission de particules rapportée à 13% d’O2(mg/Nm3)
         # TODO V1 / (≤40 si bûches ≤ 30 pour granulé)
 
-        add_error("emission_nox_poele_manquant", geste) if geste[:emission_Nox].blank?
+        add_error("poele_emission_nox_manquant", geste) if geste[:emission_Nox].blank?
         # Emission d’oxydes d’azotes (NOx) rapporté à 13% d’O2 (mg/Nm3)
         # TODO V1 / (≤ 200 pour les deux)
       end
@@ -107,17 +107,17 @@ module QuoteValidator
       def validate_systeme_solaire_combine(geste)
         validate_chauffage(geste)
 
-        add_error("marque_capteurs_manquant", geste) if geste[:marque_capteurs].blank?
-        add_error("reference_capteurs_manquant", geste) if geste[:reference_capteurs].blank?
-        add_error("type_capteurs_manquant", geste) if geste[:type_capteurs].blank?
-        add_error("surface_captage_manquant", geste) if geste[:surface_captage].blank? # m2
+        add_error("ssc_marque_capteurs_manquant", geste) if geste[:marque_capteurs].blank?
+        add_error("ssc_reference_capteurs_manquant", geste) if geste[:reference_capteurs].blank?
+        add_error("ssc_type_capteurs_manquant", geste) if geste[:type_capteurs].blank?
+        add_error("ssc_surface_captage_manquant", geste) if geste[:surface_captage].blank? # m2
         # Todo V1 : Vérifier valeur
         # ≥ 6m2 MPR
         # ≥ 8m2 CEE
 
-        add_error("productivite_capteurs_manquant", geste) if geste[:productivite_capteurs].blank? # W/m2
+        add_error("ssc_productivite_capteurs_manquant", geste) if geste[:productivite_capteurs].blank? # W/m2
         # TODO : Que CEE ? V1, vérifier valeur : ≥ 600 W/m2
-        add_error("volume_ballon_manquant", geste) if geste[:volume_ballon].blank?
+        add_error("ssc_volume_ballon_manquant", geste) if geste[:volume_ballon].blank?
         # (peut être associé à plusieurs ballons)
         # >300L MPR
         # >400L CEE
@@ -126,7 +126,7 @@ module QuoteValidator
 
         # TODO: V1 : profil de soutirage
 
-        add_error("energie_appoint_manquant", geste) if geste[:energie_appoint].blank? # electricité, gaz...
+        add_error("ssc_energie_appoint_manquant", geste) if geste[:energie_appoint].blank? # electricité, gaz...
 
         # TODO: V1 :valeur ETAS
         # ≥ 82% si EES de l’appoint séparé inférieur à 82 %
@@ -140,16 +140,16 @@ module QuoteValidator
         validate_chauffage(geste)
 
         # air-eau, eau-eau, air-air, hybride -> TODO Verifier si besoin de l'indication sur le devis
-        add_error("type_pac_manquant", geste) if geste[:type].blank?
-        add_error("regime_temperature_manquant", geste) if geste[:regime_temperature].blank? # basse, moyenne, haute
+        add_error("pac_type_manquant", geste) if geste[:type].blank?
+        add_error("pac_regime_temperature_manquant", geste) if geste[:regime_temperature].blank? # basse, moyenne, haute
         # R410A -  attention, celui ci va être restreint, R32 …
-        add_error("type_fluide_frigorigene_manquant", geste) if geste[:type_fluide_frigorigene].blank?
+        add_error("pac_type_fluide_frigorigene_manquant", geste) if geste[:type_fluide_frigorigene].blank?
 
         # TODO: V1, verifier valeur ETAS :
         # ≥ 126% si basse T
         # ≥ 111% si Haute T
 
-        add_error("cop_chauffage_manquant", geste) if geste[:COP].blank? # TODO: V1 Check if SCOP is required too.
+        add_error("pac_cop_chauffage_manquant", geste) if geste[:COP].blank? # TODO: V1 Check if SCOP is required too.
       end
 
       protected
