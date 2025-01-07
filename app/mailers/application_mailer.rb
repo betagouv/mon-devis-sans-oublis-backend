eea0f1a8fde7660010103946f3439e92be977c5e
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-01-07 09:37:35 +0100

    chore: better dealing with App env

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -7,11 +7,8 @@ class ApplicationMailer < ActionMailer::Base
   def subject(text)
     subject_parts = ["[#{Rails.application.config.application_name}]"]
 
-    if ENV.key?("APP_ENV") && ENV["APP_ENV"] != "production"
-      subject_parts << "[#{ENV.fetch('APP_ENV')}]"
-    elsif !Rails.env.production?
-      subject_parts << "[#{Rails.env}]"
-    end
+    specific_env = [Rails.application.config.app_env, Rails.env] - ["production"]
+    subject_parts << "[#{specific_env}]" if specific_env
 
     subject_parts << text
 
