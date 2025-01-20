18e721e31a0f1587e169afac1b97c15a7d19f413
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-01-20 17:20:21 +0100

    fix: better LLM result filtering

--- spec/lib/llms/base_spec.rb
+++ spec/lib/llms/base_spec.rb
@@ -4,13 +4,18 @@ require "rails_helper"
 
 RSpec.describe Llms::Base, type: :service do
   describe ".clean_value" do
+    # rubocop:disable RSpec/ExampleLength
     it "removes empty mentions" do # rubocop:disable RSpec/MultipleExpectations
       expect(described_class.clean_value(" Non mentionné ")).to be_nil
       expect(described_class.clean_value("Non disponible")).to be_nil
       expect(described_class.clean_value("Aucun IBAN n'est mentionné.")).to be_nil
+      expect(described_class.clean_value("Aucun IBAN mentionné.")).to be_nil
+      expect(described_class.clean_value("Aucun label mentionné")).to be_nil
       expect(described_class.clean_value("Inconnu (pas de nom de client)")).to be_nil
       expect(described_class.clean_value("Aucune")).to be_nil
+      expect(described_class.clean_value("aucune mention")).to be_nil
     end
+    # rubocop:enable RSpec/ExampleLength
   end
 
   describe ".extract_numbered_list" do
