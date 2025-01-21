1f11146978ffbfe77a7a647d59f649c6793a7ea3
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-01-21 12:23:27 +0100

    fix: allow all CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,14 +2,15 @@
 
 require_relative "../../lib/uri_extended"
 
-frontend_origins = if Rails.env.production?
-                     [
-                       UriExtended.host_with_port(ENV.fetch("FRONTEND_APPLICATION_HOST")),
-                       Rails.application.config.app_env == "staging" ? "localhost" : nil
-                     ].compact
-                   else
-                     "*"
-                   end
+# frontend_origins = if Rails.env.production?
+#                      [
+#                        UriExtended.host_with_port(ENV.fetch("FRONTEND_APPLICATION_HOST")),
+#                        Rails.application.config.app_env == "staging" ? "localhost" : nil
+#                      ].compact
+#                    else
+#                      "*"
+#                    end
+frontend_origins = "*" # TODO: Remove me security issue
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
