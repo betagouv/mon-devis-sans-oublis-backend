8ded5c91cbd70daec3b00c00f36493563f7a1bc3
Martin Letellier (Beta.gouv) <dinum-327867@Martin-Letellier---Mon-Devis-Sans-Oublis.local>
2025-01-27 21:06:04 +0100

    vitrage except volet isolant

--- lib/quote_validator/works/menuiserie.rb
+++ lib/quote_validator/works/menuiserie.rb
@@ -15,7 +15,6 @@ module QuoteValidator
         add_error("menuiserie_marque_manquant", geste) if geste[:marque].blank?
         add_error("menuiserie_reference_manquant", geste) if geste[:reference].blank?
         add_error("menuiserie_type_materiau_manquant", geste) if geste[:type_materiaux].blank? # bois, alu, pvc ...
-        add_error("menuiserie_type_vitrage_manquant", geste) if geste[:type_vitrage].blank? # simple - double vitrage
         add_error("menuiserie_type_pose_manquant", geste) if geste[:type_pose].blank? # renovation ou depose totale
         add_error("menuiserie_localisation_manquant", geste) if geste[:localisation].blank?
         add_error("menuiserie_position_paroie_manquant", geste) if geste[:position_paroie].blank?
@@ -26,6 +25,7 @@ module QuoteValidator
 
       def validate_menuiserie_fenetre(geste)
         validate_menuiserie(geste)
+        add_error("menuiserie_type_vitrage_manquant", geste) if geste[:type_vitrage].blank? # simple - double vitrage
         add_error("menuiserie_uw_fenetre_manquant", geste) if geste[:uw].blank?
         add_error("menuiserie_sw_fenetre_manquant", geste) if geste[:sw].blank?
         # V1, check valeurs : Uw ≤ 1,3 W/m².K et Sw ≥ 0,3 OU Uw ≤ 1,7 W/m².K et Sw ≥ 0,36
@@ -33,6 +33,7 @@ module QuoteValidator
 
       def validate_menuiserie_fenetre_toit(geste)
         validate_menuiserie(geste)
+        add_error("menuiserie_type_vitrage_manquant", geste) if geste[:type_vitrage].blank? # simple - double vitrage
         add_error("menuiserie_uw_fenetre_toit_manquant", geste) if geste[:uw].blank?
         add_error("menuiserie_sw_fenetre_toit_manquant", geste) if geste[:sw].blank?
         # V1, check valeurs : (Uw ≤ 1,5 W/m².K et Sw ≤ 0,36 )
@@ -40,6 +41,7 @@ module QuoteValidator
 
       def validate_menuiserie_porte(geste)
         validate_menuiserie(geste)
+        add_error("menuiserie_type_vitrage_manquant", geste) if geste[:type_vitrage].blank? # simple - double vitrage
         add_error("menuiserie_ud_porte_manquant", geste) if geste[:ud].blank? # TODO : Que CEE ?
         # v1, check valeurs : Ud ≤ 1,7 W/m².K
       end
