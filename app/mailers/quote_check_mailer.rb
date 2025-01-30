ef1a90642562ef1eddd88e8b7d4e0e6a1a40335e
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-01-30 14:43:48 +0100

    fix: empty admin_recipients for mails

--- app/mailers/quote_check_mailer.rb
+++ app/mailers/quote_check_mailer.rb
@@ -4,13 +4,18 @@ class QuoteCheckMailer < ApplicationMailer
   def created(quote_check)
     @quote_check = quote_check
 
-    recipients = ENV["QUOTE_CHECK_EMAIL_RECIPIENTS"]&.split(",")
-    return if recipients.blank?
+    return if admin_recipients.blank?
 
     mail(
       to: recipients.first,
       cc: recipients[1..],
       subject: subject("Nouveau devis soumis #{quote_check.id}")
     )
+  end
+
+  private
+
+  def admin_recipients
+    @admin_recipients ||= ENV["QUOTE_CHECK_EMAIL_RECIPIENTS"]&.strip&.split(",")
   end
 end
