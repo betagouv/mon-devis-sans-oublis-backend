85b3a9f045c32ff50cf0e2f40a3ff6d6298a62ca
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-01-30 09:09:50 +0100

    chore: ensure UUID as default ID type

--- config/application.rb
+++ config/application.rb
@@ -49,6 +49,9 @@ module MesDevisSansOublis
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
 
     config.autoload_lib(ignore: %w[assets tasks])
 
