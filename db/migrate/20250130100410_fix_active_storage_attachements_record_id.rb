38a1b02cf44581fa6fe0b897d7ba7bacab59089f
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-01-30 12:12:58 +0100

    fix: ActiveStorage::Attachment record_id

---
+++ db/migrate/20250130100410_fix_active_storage_attachements_record_id.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class FixActiveStorageAttachementsRecordId < ActiveRecord::Migration[7.2]
+  def up
+    remove_index :active_storage_attachments, name: "index_active_storage_attachments_uniqueness"
+    remove_column :active_storage_attachments, :record_id
+
+    add_column :active_storage_attachments, :record_id, :uuid
+  end
+
+  def down
+    remove_column :active_storage_attachments, :record_id, :uuid
+
+    add_column :active_storage_attachments, :record_id, :bigint
+    add_index :active_storage_attachments, %w[record_type record_id name blob_id],
+              name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+end
