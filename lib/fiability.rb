72bfb297f4f865f66f67c48d678f533fe52bad73
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-02-04 16:18:17 +0100

    fix: lint

--- lib/fiability.rb
+++ lib/fiability.rb
@@ -2,10 +2,7 @@
 
 require "diff/lcs"
 
-# (0...max_errors_count).to_a.sum do |index|
-#   new_quote_check.validation_errors[index] == source_quote_check.expected_validation_errors[index] ? 0 : 1
-# end
-
+# Tools for Fiability
 class Fiability
   def self.count_differences(array1, array2)
     changes = Diff::LCS.sdiff(array1, array2)
