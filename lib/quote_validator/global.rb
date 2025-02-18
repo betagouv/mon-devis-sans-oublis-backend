2dc2d20be2e90c962ccb276d4fbd62fef6b65ed7
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-02-18 15:13:45 +0100

    fix: lint

--- lib/quote_validator/global.rb
+++ lib/quote_validator/global.rb
@@ -30,6 +30,7 @@ module QuoteValidator
     # date d'emission, date de pré-visite (CEE uniquement ?),
     # validité (par défaut 3 mois -> Juste un warning),
     # Date de début de chantier (CEE uniquement)
+    # rubocop:disable Metrics/AbcSize
     def validate_dates
       # date_devis
       add_error("date_devis_manquant", category: "admin", type: "missing") if quote[:date_devis].blank?
@@ -37,7 +38,9 @@ module QuoteValidator
       # date_debut_chantier
       date_chantier = quote[:date_debut_chantier]
       delai_debut_chantier = quote[:delai_debut_chantier]
-      add_error("date_chantier_manquant", category: "admin", type: "warning") if date_chantier.blank? && delai_debut_chantier.blank?
+      if date_chantier.blank? && delai_debut_chantier.blank?
+        add_error("date_chantier_manquant", category: "admin", type: "warning")
+      end
 
       # date_pre_visite
       add_error("date_pre_visite_manquant", category: "admin", type: "warning") if quote[:date_pre_visite].blank?
@@ -45,6 +48,7 @@ module QuoteValidator
       # validite
       add_error("date_validite_manquant", category: "admin", type: "warning") unless quote[:validite]
     end
+    # rubocop:enable Metrics/AbcSize
 
     # V0 on check la présence - attention devrait dépendre du geste, à terme,
     # on pourra utiliser une API pour vérifier la validité
