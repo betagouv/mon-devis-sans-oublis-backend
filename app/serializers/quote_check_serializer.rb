c1b942126560dc2e4392a14ad47be1c92b5c45aa
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-02-19 11:39:10 +0100

    chore: clean Serializer

--- app/serializers/quote_check_serializer.rb
+++ app/serializers/quote_check_serializer.rb
@@ -1,60 +1,49 @@
 # frozen_string_literal: true
 
 class QuoteCheckSerializer < ActiveModel::Serializer
-  # attributes :id, :status, :created_at # TODO
+  attributes :id, :status, :profile, :metadata,
+             :errors, :error_details, :error_messages,
+             :parent_id,
+             :filename,
+             :gestes,
+             :finished_at,
+             # Virtual attributes
+             :errors, :error_details, :error_messages,
+             :gestes
 
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/PerceivedComplexity
-  def as_json(*args)
-    json_hash = super.merge(object.attributes.merge({ # Warning: attributes has stringifed keys, so use it too
-                                                      "status" => object.status,
-                                                      "errors" => object.validation_errors,
-                                                      "error_details" => object.validation_error_details&.map do
-                                                        it.merge(
-                                                          "comment" => object.validation_error_edits&.fetch(
-                                                            it["id"], {}
-                                                          )&.[]("comment") || nil,
-                                                          "deleted" =>
-                                                           object.validation_error_edits&.fetch(
-                                                             it["id"], {}
-                                                           )&.[]("deleted") || false
-                                                        )
-                                                      end,
-                                                      "error_messages" => object.validation_errors&.index_with do
-                                                        I18n.t("quote_validator.errors.#{it}")
-                                                      end,
-                                                      "filename" => object.filename,
-
-                                                      "gestes" => object.read_attributes&.fetch("gestes", nil) # rubocop:disable Style/SafeNavigationChainLength
-                     &.map&.with_index do |geste, geste_index|
-                       geste_id = QuoteValidator::Base.geste_index(
-                         object.id, geste_index
-                       )
-
-                       geste.slice("intitule").merge(
-                         "id" => geste_id,
-                         "valid" =>
-                           object.validation_error_details.none? do
-                             it["geste_id"] == geste_id
-                           end
-                       )
-                     end
-                                                    }))
-    return json_hash if Rails.env.development?
-
-    json_hash.slice(
-      "id", "status", "profile", "metadata",
-      "valid", "errors", "error_details", "error_messages",
-      "parent_id",
-      "filename",
-      "gestes",
-      "finished_at"
-    ).compact
+  def attributes(*args)
+    super.compact # Removes keys with nil values
   end
-  # rubocop:enable Metrics/PerceivedComplexity
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/CyclomaticComplexity
-  # rubocop:enable Metrics/AbcSize
+
+  def errors
+    object.validation_errors
+  end
+
+  def error_details
+    object.validation_error_details&.map do
+      it.merge(
+        "comment" => object.validation_error_edits&.dig(it["id"], "comment"),
+        "deleted" => object.validation_error_edits&.dig(it["id"], "deleted") || false
+      ).compact
+    end
+  end
+
+  def error_messages
+    object.validation_errors&.index_with do
+      I18n.t("quote_validator.errors.#{it}")
+    end
+  end
+
+  def gestes
+    object.read_attributes&.fetch("gestes", nil)&.map&.with_index do |geste, geste_index| # rubocop:disable Style/SafeNavigationChainLength
+      geste_id = QuoteValidator::Base.geste_index(object.id, geste_index)
+      geste.slice("intitule").merge(
+        "id" => geste_id,
+        "valid" =>
+          object.validation_error_details.none? do
+            it["geste_id"] == geste_id
+          end
+      )
+    end
+  end
 end
