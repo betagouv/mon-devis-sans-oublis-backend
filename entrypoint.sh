e363ca7f5a56ae182d6f22752ea1fa462d56c44c
nicolasleger <570901+nicolasleger@users.noreply.github.com>
2025-02-25 16:32:27 +0100

    fix(CI): Docker entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,10 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
-bin/rails db:create
-
 if [ -n "$SILENT_MIGRATION" ]; then
-  bin/rails db:migrate > /dev/null
+  bin/rails db:create && bin/rails db:migrate > /dev/null
 else
-  bin/rails db:migrate
+  bin/rails db:create && bin/rails db:migrate
 fi
 
 exec "$@"
